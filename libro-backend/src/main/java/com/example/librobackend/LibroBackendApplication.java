e65100b3ba54ebe4d00ddf3b09289196d38e9acf
matias <matias_gonzalez@javeriana.edu.co>
2024-05-21 18:23:55 -0500

    Springboot

---
+++ libro-backend/src/main/java/com/example/librobackend/LibroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.librobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroBackendApplication.class, args);
+	}
+
+}
---
+++ libro-backend/src/test/java/com/example/librobackend/LibroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
